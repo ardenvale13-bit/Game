628be95f8016923138adc602270462ec0a241324
Arden Vale <ardenvale13@gmail.com>
2026-04-25 13:32:07 +1200

    fe

--- src/data/avatars.ts
+++ src/data/avatars.ts
@@ -1,101 +1,54 @@
-// All 86 custom avatars for Game Time
+// All 34 custom avatars for Game Time
 // Organized by category for easier browsing
 
 export interface Avatar {
   id: string;
   filename: string;
   name: string;
-  category: 'hearts' | 'animals' | 'murder-crew' | 'cats' | 'people' | 'nature' | 'misc';
+  category: 'animals' | 'cats' | 'gg-crew' | 'misc';
 }
 
 export const avatars: Avatar[] = [
-  // === MURDER CREW (the iconic trio + llama) ===
-  { id: 'murder_duck', filename: 'murder_duck.png', name: 'Murder Duck', category: 'murder-crew' },
-  { id: 'murder_hippo', filename: 'murder_hippo_icon.png', name: 'Murder Hippo', category: 'murder-crew' },
-  { id: 'murder_lotyl', filename: 'murder_lotyl_icon.png', name: 'Murder Lotyl', category: 'murder-crew' },
-  { id: 'stabby_llama', filename: 'stabby_llama_icon.png', name: 'Stabby Llama', category: 'murder-crew' },
-
-  // === HEARTS ===
-  { id: 'heart_neon', filename: 'ainsanity9_icon.png', name: 'Heart Neon', category: 'hearts' },
-  { id: 'heart_violet', filename: 'ainsanity10_icon.png', name: 'Heart Violet', category: 'hearts' },
-  { id: 'heart_sunset', filename: 'ainsanity11_icon.png', name: 'Heart Sunset', category: 'hearts' },
-  { id: 'heart_white', filename: 'ainsanity12_icon.png', name: 'Heart White', category: 'hearts' },
-  { id: 'heart_icon', filename: 'heart-icon.png', name: 'Heart', category: 'hearts' },
-  { id: 'heart2_icon', filename: 'heart2-icon.png', name: 'Heart 2', category: 'hearts' },
-
   // === CATS ===
   { id: 'catto', filename: 'catto-icon.png', name: 'Catto', category: 'cats' },
-  { id: 'cat', filename: 'cat_icon.png', name: 'Ghost Cat', category: 'cats' },
   { id: 'cat2', filename: 'cat2_icon.png', name: 'Grey Cat', category: 'cats' },
   { id: 'cat3', filename: 'cat3_icon.png', name: 'Orange Cat', category: 'cats' },
-  { id: 'cat4', filename: 'cat4_icon.png', name: 'Grumpy Cat', category: 'cats' },
   { id: 'cat5', filename: 'cat5_icon.png', name: 'Cat Pile', category: 'cats' },
-  { id: 'magrat', filename: 'magrat_icon.png', name: 'Magrat', category: 'cats' },
   { id: 'zibb', filename: 'zibb_icon.png', name: 'Zibb', category: 'cats' },
 
   // === ANIMALS ===
   { id: 'axolotyl', filename: 'axolotyl_icon.png', name: 'Axolotl', category: 'animals' },
-  { id: 'bat', filename: 'bat_icon.png', name: 'Bat', category: 'animals' },
-  { id: 'birb', filename: 'birb_icon.png', name: 'Birb', category: 'animals' },
-  { id: 'chimkin', filename: 'chimkin_icon.png', name: 'Chimkin', category: 'animals' },
-  { id: 'corvi', filename: 'corvi_icon.png', name: 'Corvi', category: 'animals' },
-  { id: 'corvi1', filename: 'corvi1_icon.png', name: 'Corvi Purple', category: 'animals' },
-  { id: 'cow', filename: 'cow_icon.png', name: 'Cow', category: 'animals' },
-  { id: 'dean', filename: 'dean_icon.png', name: 'Dean', category: 'animals' },
-  { id: 'dino', filename: 'dino_icon.png', name: 'Dino', category: 'animals' },
-  { id: 'dino1', filename: 'dino1_icon.png', name: 'Dino TP', category: 'animals' },
-  { id: 'duck', filename: 'duck_icon.png', name: 'Shy Duck', category: 'animals' },
-  { id: 'elephant', filename: 'elephant_icon.png', name: 'Elephant', category: 'animals' },
-  { id: 'frog', filename: 'frog_icon.png', name: 'Space Frog', category: 'animals' },
-  { id: 'hamster', filename: 'hamster_icon.png', name: 'Hamster', category: 'animals' },
-  { id: 'jellyfish', filename: 'jellyfish_icon.png', name: 'Jellyfish', category: 'animals' },
-  { id: 'koala', filename: 'koala_icon.png', name: 'Koala', category: 'animals' },
-  { id: 'otter', filename: 'otter_icon.png', name: 'Otter', category: 'animals' },
-  { id: 'otter2', filename: 'otter_2_icon.png', name: 'Happy Otter', category: 'animals' },
-  { id: 'panda', filename: 'panda_icon.png', name: 'Panda', category: 'animals' },
-  { id: 'penguin', filename: 'penguin_icon.png', name: 'Penguin', category: 'animals' },
-  { id: 'pig', filename: 'pig_icon.png', name: 'Pig', category: 'animals' },
-  { id: 'platy', filename: 'platy_icon.png', name: 'Platy', category: 'animals' },
-  { id: 'screamy_possum', filename: 'screamy_possum_icon.png', name: 'Screamy Possum', category: 'animals' },
-  { id: 'sheepy', filename: 'sheepy_icon.png', name: 'Sheepy', category: 'animals' },
-  { id: 'tiger', filename: 'tiger_icon.png', name: 'Tiger', category: 'animals' },
-  { id: 'tortl', filename: 'tortl_icon.png', name: 'Tortl', category: 'animals' },
-  { id: 'wolf', filename: 'wolf_icon.png', name: 'Wolf', category: 'animals' },
   { id: 'bunny', filename: 'bunny-icon.png', name: 'Bunny', category: 'animals' },
   { id: 'capybara', filename: 'capybara-icon.png', name: 'Capybara', category: 'animals' },
-  { id: 'dino2', filename: 'dino2-icon.png', name: 'Dino 2', category: 'animals' },
-  { id: 'flamingo', filename: 'flamingo-icon.png', name: 'Flamingo', category: 'animals' },
+  { id: 'chimkin', filename: 'chimkin_icon.png', name: 'Chimkin', category: 'animals' },
+  { id: 'corvi1', filename: 'corvi1_icon.png', name: 'Corvi', category: 'animals' },
+  { id: 'cow', filename: 'cow_icon.png', name: 'Cow', category: 'animals' },
+  { id: 'dino2', filename: 'dino2-icon.png', name: 'Dino', category: 'animals' },
+  { id: 'duck', filename: 'duck_icon.png', name: 'Shy Duck', category: 'animals' },
+  { id: 'murder_duck', filename: 'murder_duck.png', name: 'Murder Duck', category: 'animals' },
+  { id: 'otter2', filename: 'otter_2_icon.png', name: 'Otter', category: 'animals' },
+  { id: 'pibble', filename: 'pibble-icon.png', name: 'Pibble', category: 'animals' },
+  { id: 'racoon', filename: 'racoon_icon.png', name: 'Racoon', category: 'animals' },
+  { id: 'screamy_possum', filename: 'screamy_possum_icon.png', name: 'Screamy Possum', category: 'animals' },
+  { id: 'seren', filename: 'seren-icon.png', name: 'Seren', category: 'animals' },
+  { id: 'sheepy', filename: 'sheepy_icon.png', name: 'Sheepy', category: 'animals' },
   { id: 'snek', filename: 'snek-icon.png', name: 'Snek', category: 'animals' },
-  { id: 'snowlep', filename: 'snowlep-icon.png', name: 'Snow Leopard', category: 'animals' },
-  { id: 'yak', filename: 'yak-icon.png', name: 'Yak', category: 'animals' },
+  { id: 'snell', filename: 'snell-icon.png', name: 'Snell', category: 'animals' },
+  { id: 'tortl', filename: 'tortl_icon.png', name: 'Tortl', category: 'animals' },
   { id: 'unicorn', filename: 'unicorn-icon.png', name: 'Unicorn', category: 'animals' },
+  { id: 'wolf', filename: 'wolf_icon.png', name: 'Wolf', category: 'animals' },
 
-  // === PEOPLE ===
-  { id: 'boy', filename: 'boy_icon.png', name: 'Boy', category: 'people' },
-  { id: 'girl', filename: 'girl_face_icon.png', name: 'Girl', category: 'people' },
-  { id: 'girl2', filename: 'girl_2_icon.png', name: 'Curly Girl', category: 'people' },
-  { id: 'grandma', filename: 'grandma_icon.png', name: 'Grandma', category: 'people' },
-  { id: 'lady', filename: 'lady_icon.png', name: 'Lady', category: 'people' },
-  { id: 'man', filename: 'man_icon.png', name: 'Suit Man', category: 'people' },
-  { id: 'man2', filename: 'man2_icon.png', name: 'Beard Man', category: 'people' },
-  { id: 'woman', filename: 'woman_icon.png', name: 'Woman', category: 'people' },
-  { id: 'woman2', filename: 'woman2_icon.png', name: 'Curly Woman', category: 'people' },
-  { id: 'woman3', filename: 'woman3_icon.png', name: 'Elegant Woman', category: 'people' },
-  { id: 'woman4', filename: 'woman4_icon.png', name: 'Hijabi', category: 'people' },
-
-  // === NATURE ===
-  { id: 'flower', filename: 'flower_icon.png', name: 'Purple Flower', category: 'nature' },
-  { id: 'flower2', filename: 'flower2_icon.png', name: 'Blue Flower', category: 'nature' },
-  { id: 'flower3', filename: 'flower3_icon.png', name: 'Happy Flower', category: 'nature' },
-  { id: 'lavender', filename: 'lavender_icon.png', name: 'Lavender', category: 'nature' },
+  // === GG CREW ===
+  { id: 'gg', filename: 'gg-icon.png', name: 'GG', category: 'gg-crew' },
+  { id: 'gg_blep', filename: 'gg-blep-icon.png', name: 'GG Blep', category: 'gg-crew' },
+  { id: 'gg_dino', filename: 'gg-dino-icon.png', name: 'GG Dino', category: 'gg-crew' },
+  { id: 'gg_fightme', filename: 'gg-fightme-icon.png', name: 'GG Fight Me', category: 'gg-crew' },
+  { id: 'gg_shark', filename: 'gg-shark-icon.png', name: 'GG Shark', category: 'gg-crew' },
+  { id: 'gg_stfu', filename: 'gg-stfu-icon.png', name: 'GG STFU', category: 'gg-crew' },
+  { id: 'evil_gg', filename: 'evil-gg-icon.png', name: 'Evil GG', category: 'gg-crew' },
 
   // === MISC ===
-  { id: 'face', filename: 'face_icon.png', name: 'Cursed Face', category: 'misc' },
-  { id: 'lau', filename: 'lau_icon.png', name: 'Friends', category: 'misc' },
-  { id: 'robo', filename: 'robo_icon.png', name: 'Robo', category: 'misc' },
-  { id: 'skull', filename: 'skull_icon.png', name: 'Floral Skull', category: 'misc' },
-  { id: 'skull2', filename: 'skull2_icon.png', name: 'Cute Skull', category: 'misc' },
-  { id: 'smile', filename: 'smile_icon.png', name: 'Heart Eyes', category: 'misc' },
+  { id: 'arden_rat', filename: 'arden-rat-icon.png', name: 'Arden Rat', category: 'misc' },
   { id: 'finn', filename: 'finn-icon.png', name: 'Finn', category: 'misc' },
 ];
 
@@ -116,22 +69,16 @@ export function getRandomAvatar(): Avatar {
 
 // Category display names
 export const categoryNames: Record<Avatar['category'], string> = {
-  'murder-crew': '🔪 Murder Crew',
-  'hearts': '💜 Hearts',
-  'cats': '🐱 Cats',
-  'animals': '🐾 Animals',
-  'people': '👤 People',
-  'nature': '🌸 Nature',
-  'misc': '✨ Misc',
+  'cats': 'Cats',
+  'animals': 'Animals',
+  'gg-crew': 'GG Crew',
+  'misc': 'Misc',
 };
 
 // Category order for display
 export const categoryOrder: Avatar['category'][] = [
-  'murder-crew',
-  'hearts', 
   'cats',
   'animals',
-  'people',
-  'nature',
+  'gg-crew',
   'misc',
 ];
